6343406fb223b55bf19ec1c1ddbcbf64da3573bd
charlie-wt <shout@charlies.computer>
2026-01-07 16:25:13 +0000

    vim.bash: point to local `vim` via `$PATH` & `$VIMRUNTIME`

--- bash/vim.bash
+++ bash/vim.bash
@@ -1,7 +1,6 @@
 vim_src_dir="$HOME/src/bin/vim"
 [ -x "$vim_src_dir/src/vim" ] || return 2
 
-# note: this is a function, not an alias, so that we can use it from scripts
-vim () {
-    VIMRUNTIME=$vim_src_dir/runtime $vim_src_dir/src/vim $@
-}
+# use locally-built vim, if it exists
+export VIMRUNTIME="$vim_src_dir/runtime"
+export PATH="$vim_src_dir/src:$PATH"
