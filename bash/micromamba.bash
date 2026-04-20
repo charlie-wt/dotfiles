5edafe1f3d871443a0dac849db05e35335009019
charlie-wt <shout@charlies.computer>
2026-04-20 17:41:18 +0100

    install,micromamba.bash: (hopefully) install bash completion scripts

--- bash/micromamba.bash
+++ bash/micromamba.bash
@@ -15,12 +15,28 @@ unset __setup_file
 
 
 
-# MINE: register manpages from `global` environment
-global_env="$MAMBA_ROOT_PREFIX/envs/global"
-if [ -d "$global_env" ]; then
-    [ -d "$global_env/share/man" ] && export MANPATH="$MANPATH:$global_env/share/man"
-    # NOTE: allowing running micromamba bins 'globally' is best done with wrapper
-    # scripts (so that they work with eg. tools that expect names of executables).
-    # there's infra to do this in `install`, so if that's needed elsewhere best to copy
-    # it out of there into a standalone script.
-fi
+# MY STUFF (use a function just to be able to use `local`)
+__mine () {
+    local global_env="$MAMBA_ROOT_PREFIX/envs/global"
+    if [ -d "$global_env" ]; then
+        # register manpages from `global` environment
+        [ -d "$global_env/share/man" ] && export MANPATH="$MANPATH:$global_env/share/man"
+
+        # register bash completion from `global` environment:
+        # * some packages put them in `share/bash-completion/completions`, which is nice
+        #   and can be exported straight away:
+        [ -d "$global_env/share" ] && export XDG_DATA_DIRS="$XDG_DATA_DIRS:$global_env/share"
+        # * some packages put them in `etc/bash_completion.d`, which is not nice and
+        #   forces us to create a custom dir with the right structure before exporting
+        #   (done in `install`):
+        local extra_data_dir="$MAMBA_ROOT_PREFIX/global-xdg-data-dir"
+        [ -d "$extra_data_dir" ] && export XDG_DATA_DIRS="$XDG_DATA_DIRS:$extra_data_dir"
+
+        # NOTE: allowing running micromamba bins 'globally' is best done with wrapper
+        # scripts (so that they work with eg. tools that expect names of executable
+        # files). there's infra to do this in `install`, so if that's needed elsewhere
+        # best to copy it out of there into a standalone script.
+    fi
+}
+__mine
+unset __mine
