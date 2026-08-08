cf7c646e0d0ef7d8976239bbc053fe497aa23ed2
charlie-wt <shout@charlies.computer>
2026-08-08 16:55:20 +0100

    yazi.bash: todo comment about an issue

--- bash/yazi.bash
+++ bash/yazi.bash
@@ -1,3 +1,5 @@
+# TODO #enhancement: this doesn't work for eg. opening a new tmux split from one running
+# yazi; would want the new split to open in yazi's current dir, but doesn't.
 function y () {
     local tmp="$(mktemp -t "yazi-cwd.XXXXXX")" cwd
     command yazi "$@" --cwd-file="$tmp"
